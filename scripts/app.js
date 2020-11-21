95e5383b0e7f084a9db8bdc4c8859bfbc7e49bf1
Edwyn <edwyn.3000@btinternet.com>
2020-11-21 17:30:58 +0000

    created function to add & remove Gerry

--- scripts/app.js
+++ scripts/app.js
@@ -8,18 +8,33 @@ function init() {
   const cellCount = width * width
   const cells = []
 
-  function createGrid() {
+  const gerryClass = 'gerry'
+  let gerryPosition = 94
+
+
+  // * Make a grid
+  function createGrid(startingPosition) {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cell.textContent = i
       grid.appendChild(cell)
       cells.push(cell)
     }
-    
-    
+    addGerry(startingPosition)
   }
 
-  createGrid()
+  //* Add Gerry to the grid
+  function addGerry(position) {
+    cells[position].classList.add(gerryClass)
+  }
+
+  //* Remove Gerry from the grid
+  function removeGerry(position) {
+    cells[position].classList.remove(gerryClass)
+  }
+
+
+  createGrid(gerryPosition)
 }
 
 window.addEventListener('DOMContentLoaded', init)
