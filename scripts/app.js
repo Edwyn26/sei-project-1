20dc80db8b7e02469c416f9e87bdc686b3df24ff
Edwyn <edwyn.3000@btinternet.com>
2020-11-21 16:46:45 +0000

    created the grid

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,24 @@
 
 function init() {
 
-  console.log('JS is up and running')
+  const grid = document.querySelector('.grid')
+  
+  const width = 10
+  const cellCount = width * width
+  const cells = []
 
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    
+    
+  }
+
+  createGrid()
 }
 
 window.addEventListener('DOMContentLoaded', init)
